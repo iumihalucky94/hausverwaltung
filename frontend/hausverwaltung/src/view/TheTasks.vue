d0e20a336fcd68e28574ca19738af16812504fd1
Iurie Mihalachi <iura.mihalachi@gmail.com>
2023-11-20 17:02:12 +0100

    Implemented List of Grourps and Tasks

--- frontend/hausverwaltung/src/view/TheTasks.vue
+++ frontend/hausverwaltung/src/view/TheTasks.vue
@@ -1,20 +1,26 @@
 <template>
     <section class="w-full h-full flex px-24">
-        <div class="w-full h-full">
-            List of Tasks and groups
+        <div class="w-full h-full flex justify-center items-center flex-col">
+            <p class="text-2xl underline text-thirdly">List of Groups and Tasks</p>
+            <TheGT :groupsAndTasks="data" />
         </div>
-        <div class="w-full h-full">
+        <div class="w-full h-full flex justify-center items-center">
             Create new task or group
         </div>
     </section>
 </template>
 
 <script>
+import groupsTasks from './objektgroup.json'
+import TheGT from '../components/static/groups&tasks/TheGT.vue';
 export default {
+    components: {
+        TheGT
+    },
     data() {
-
-
-        return {}
+        return {
+            data: groupsTasks
+        }
     }
 }
 </script>
